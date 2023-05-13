893baa92ec9d578b953942a667649af4bc37bfa6
Bharath S <94459111+brion4@users.noreply.github.com>
2023-05-13 14:27:46 +0530

    Add files via upload

---
+++ Exp2/pgm2-2.sh
@@ -0,0 +1,7 @@
+echo "Number upto 100 :"
+for((i=1;i<=100;i++))
+do
+	echo  -n -e '\t' "$i" 
+done
+echo -e '\n'
+
---
+++ Exp2/pgm2-4.sh
@@ -0,0 +1,12 @@
+echo "Enter number "
+read num
+digits=0
+sum=0
+while [ $num -gt 0 ]
+do
+	digit=$((num%10))
+	sum=$((sum+digit))
+	num=$((num/10))
+done
+echo "Sum of digits is $sum"
+	
